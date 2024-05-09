945166189167f1ba45294fa8acf6535ca0ac37dc
Artyom Narshinov <narshinovartyom@mail.ru>
2024-05-09 19:45:40 +0400

    asdasd

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
-require('dotenv').config();
-const express = require('express');
-const morgan = require('morgan');
-const path = require('path');
-const cors = require('cors');
-const indexRouter = require('./routes/index.router');
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
+const cors = require("cors");
+const indexRouter = require("./routes/index.router");
 const app = express();
 const { PORT } = process.env;
 const corsOptions = {
-    origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
-    credentials: true,
-    methods: ['GET', 'POST', 'DELETE', 'PATCH', 'PUT'],
+	origin: [
+		"http://localhost:3000",
+		"http://127.0.0.1:3000",
+		"https://663cef1a2835740626755689--lively-starlight-8ad587.netlify.app/",
+	],
+	credentials: true,
+	methods: ["GET", "POST", "DELETE", "PATCH", "PUT"],
 };
 
 app.use(cors(corsOptions));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(morgan('dev'));
-app.use(express.static(path.join(process.cwd(), 'public')));
+app.use(morgan("dev"));
+app.use(express.static(path.join(process.cwd(), "public")));
 
-app.use('/api', indexRouter);
+app.use("/api", indexRouter);
 
-app.listen(PORT, () => console.log(`Сервер запущен: http://localhost:${PORT}`))
+app.listen(PORT, () => console.log(`Сервер запущен: http://localhost:${PORT}`));
